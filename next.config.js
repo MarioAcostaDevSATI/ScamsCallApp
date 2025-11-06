e1ab3c271c0de7ad445018c8c860896e3c068d96
Mario Alberto Acosta Priero <est.marioa.acosta@unimilitar.edu.co>
2025-11-06 05:26:14 -0500

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,29 +1,15 @@
-{
-  "compilerOptions": {
-    "target": "es5",
-    "lib": ["dom", "dom.iterable", "es6"],
-    "allowJs": true,
-    "skipLibCheck": true,
-    "strict": true,
-    "forceConsistentCasingInFileNames": true,
-    "noEmit": true,
-    "esModuleInterop": true,
-    "module": "esnext",
-    "moduleResolution": "bundler",
-    "resolveJsonModule": true,
-    "isolatedModules": true,
-    "jsx": "preserve",
-    "incremental": true,
-    "plugins": [
-      {
-        "name": "next"
-      }
-    ],
-    "baseUrl": ".",
-    "paths": {
-      "@/*": ["./src/*"]
-    }
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export',
+  trailingSlash: true,
+  basePath: '/ScamsCallApp',
+  assetPrefix: '/ScamsCallApp/',
+  images: {
+    unoptimized: true
   },
-  "include": ["next-env.d.ts", "**/*.ts", "**/*.tsx", ".next/types/**/*.ts"],
-  "exclude": ["node_modules"]
+  experimental: {
+    appDir: true,
+  },
 }
+
+module.exports = nextConfig
