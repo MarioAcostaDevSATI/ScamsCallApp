a5acd6cff1bbeb2cfe1f63acbd8eb8a9ffb9f301
Mario Alberto Acosta Priero <est.marioa.acosta@unimilitar.edu.co>
2025-11-05 20:20:32 -0500

    Create page.tsx

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <main>
+      <h1>Scams Call Colombia</h1>
+      <p>Plataforma para reportar estafas telefónicas</p>
+      <button>Reportar Estafa</button>
+    </main>
+  )
+}
